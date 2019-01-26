744ac173174170b0db34e791011282b06ccb0187
Matthew Kataryniak <mwk9@njit.edu>
2019-01-26 12:12:20 -0500

    added enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character
+{
+    public bool isSelectable;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSelectable)
+        {
+            sr.color = Color.yellow;
+        }
+        else
+        {
+            sr.color = Color.white;
+        }
+    }
+
+    public override void TakeDamage(int damageAmount)
+    {
+        currHealth -= damageAmount;
+        //check if dead
+    }
+}
