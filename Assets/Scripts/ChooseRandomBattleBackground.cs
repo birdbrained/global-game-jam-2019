f25995ccccb7423aa8a33e29f06e01ff67ec3c79
Matthew Kataryniak <mwk9@njit.edu>
2019-01-26 20:45:18 -0500

    merged ethan's shaders into the game

---
+++ Assets/Scripts/ChooseRandomBattleBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseRandomBattleBackground : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject backgroundObj;
+    //private Material backgroundObjMaterial;
+    [SerializeField]
+    private Material[] backgroundMaterials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (backgroundObj != null)
+        {
+            //backgroundObjMaterial = backgroundObj.GetComponent<Material>();
+            int rando = Random.Range(0, backgroundMaterials.Length);
+            backgroundObj.GetComponent<Renderer>().material = backgroundMaterials[rando];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
